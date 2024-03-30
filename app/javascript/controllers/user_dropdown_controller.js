08564339707593bc340beadb7aab7ce170419221
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-03-30 15:26:31 +0530

    Add functionality to close navbar dropdown when clicked outside

--- app/javascript/controllers/user_dropdown_controller.js
+++ app/javascript/controllers/user_dropdown_controller.js
@@ -7,4 +7,19 @@ export default class UserController extends Controller {
     event.preventDefault()
     this.dropdownTarget.classList.toggle('hidden')
   }
+
+  outsideClick(event) {
+    // Ignore event if clicked within element
+    if(this.element === event.target || this.element.contains(event.target)) return;
+
+    // prevent any default event when dropdown is open
+    if(!this.dropdownTarget.classList.contains("hidden")) event.preventDefault();
+    
+    // close the menu on clicking outside
+    this.closeMenu()
+  }
+
+  closeMenu(){
+    this.dropdownTarget.classList.add("hidden")
+  }
 }
